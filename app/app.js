4b8ce2653b40bb330dd8550b64d36b839214dd9c
Paweł Wilkosz <pwilkosz5@gmail.com>
2019-01-06 00:11:27 +0100

    made html render on backend

--- app/app.js
+++ app/app.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const indexRouter = require('./routes/index');
 
 const app = express();
 
-// body parser
-app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 
 // loggers
 app.use(morgan('dev'));
-app.use((req, res, next) => {
-  console.info(JSON.stringify(req.body, null, 2));
-  next();
-});
+//app.use((req, res, next) => {
+//  console.info(JSON.stringify(req.body, null, 2));
+//  next();
+//});
 
 // routers
 app.use(indexRouter);
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
+
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200);
-  res.json({status: "backend works"});
-});
-router.post('/', (req, res) => {
-  res.status(200);
-  res.json({status: "backend works"});
+  res.render('pages/user/index');
 });
 
 module.exports = router;
