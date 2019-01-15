c043fa9b02655730aee2dccb47eef05823c31e46
Piotr Kołodziejczyk <44337579+pi-kolo@users.noreply.github.com>
2019-01-15 01:03:29 +0100

    Just some selects

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -15,4 +15,20 @@ async function test() {
   return result;
 }
 
-module.exports = {test};
+async function filmsOnDay(day, cinema) {	
+  const connection = await mariadb.createConnection(credentials);
+  const query = "select film.title, display.start_time from display join film on display.film=film.id join room on display.room=room.id where date(display.start_time)="+day+" and room.cinema = " + cinema;
+  const result =  await connection.query("");
+  connection.end();
+  return result;
+}
+
+async function allDisplaysOfFilm(film, cinema) {	
+  const connection = await mariadb.createConnection(credentials);
+  const query = "select start_time from display join room on display.room=room.id join film on display.film=film.id where room.cinema="+cinema+" and film.title=" + film; 
+  const result =  await connection.query(query);
+  connection.end();
+  return result;
+}
+
+module.exports = {test, filmsOnDay, allDisplaysOfFilm};
