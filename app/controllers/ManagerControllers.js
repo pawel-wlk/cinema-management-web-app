6cb07ae0f86ae4a461c5f08ee52b24e490b624f7
Piotr Kołodziejczyk <44337579+pi-kolo@users.noreply.github.com>
2019-01-15 17:10:01 +0100

    Some functions again

--- app/controllers/ManagerControllers.js
+++ app/controllers/ManagerControllers.js
@@ -25,7 +25,7 @@ async function allDisplaysOfFilm(film, cinema) {
 
 async function allDisplays(cinema) {	
   const connection = await mariadb.createConnection(credentials);
-  const query = "select film.title, start_time from display join room on display.room=room.id join film on display.film=film.id where room.cinema="+cinema; 
+  const query = "select id, film.title, start_time from display join room on display.room=room.id join film on display.film=film.id where room.cinema="+cinema; 
   const result =  await connection.query(query);
   connection.end();
   return result;
@@ -39,3 +39,28 @@ async function addDisplay(film, room, start_time){
   connection.end();
   return result;
 }
+
+async function updateDisplay(id, room, start_time){
+  const connection = await mariadb.createConnection(credentials);
+  const query = "update display set room = " + room +" and start_time="+start_time+"where id="+id; 
+  const result =  await connection.query(query);
+  connection.end();
+  return result;
+}
+
+async function deleteRoom (room) {
+  const connection = await.mariadb.createConnection(credentials);
+  const query = "delete from room where id="+room;
+  const result = await.connection.query(query);
+  connection.end();
+  return result;
+}	
+async function addRoom (row_num, column_num,cinema) {
+  const connection = await.mariadb.createConnection(credentials);
+  const query = "insert into room (rows_number, column_number, cinema) values ("+row_num + "," + column_num + "," + cinema + ")";
+  const result = await.connection.query(query);
+  connection.end();
+  return result;
+}	
+
+module.exports = {filmsOnDay, allDisplaysOfFilm, allDisplays, addDisplay, updateDisplay, deleteRoom, addRoom};
